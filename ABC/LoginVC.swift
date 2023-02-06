bdfbab4ea20095e0a75d50d8f8f0cb941c0aeb2a
Shubhdeep Brar <shubhdeepbrar0001@gmail.com>
2023-02-05 16:59:56 -0800

    Added login VC

---
+++ ABC/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  ABC
+//
+//  Created by Shubhdeep on 2023-02-05.
+//
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Login View controller
+        // Do any additional setup after loading the view.
+    }
+}
