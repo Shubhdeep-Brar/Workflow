c8e37ddf57dc2e344503ad5c1d86c2227275d6ea
Shubhdeep Brar <shubhdeepbrar0001@gmail.com>
2023-02-05 17:47:42 -0800

    Added new home vc

---
+++ ABC/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  ABC
+//
+//  Created by Shubhdeep on 2023-02-05.
+//
+
+import Foundation
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Login View controller
+        // Do any additional setup after loading the view.
+    }
+}
