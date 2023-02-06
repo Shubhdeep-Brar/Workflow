9fca8f383469770941b9f933f8c29f74627375f3
Shubhdeep Brar <shubhdeepbrar0001@gmail.com>
2023-02-05 17:44:00 -0800

    Added new vc

---
+++ ABC/SettingVC.swift
@@ -0,0 +1,17 @@
+//
+//  SettingVC.swift
+//  ABC
+//
+//  Created by Shubhdeep on 2023-02-05.
+//
+
+import Foundation
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Login View controller
+        // Do any additional setup after loading the view.
+    }
+}
